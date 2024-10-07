9bd2fcae0fce80d96454a09d2accc538c3adf932
Theluga <96307393+Theluga@users.noreply.github.com>
2024-10-07 19:05:27 -0300

    Delete Nemo save and restore session directory
    don't use it either

--- Nemo save and restore session/nemo_restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sleep 2
-IFS=$'\n'; read -d '' -r -a dirs < ~/nemo.dat
-
-for dir in "${dirs[@]}"; do
-    nemo "$dir" &
-    sleep 0.5
-done
--- Nemo save and restore session/nemo_save.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-declare -x DISPLAY=":0.0"
-sleep 15
-wmctrl -lx | grep nemo.Nemo && wmctrl -lx | grep nemo.Nemo | sed -e "s#Dossier\ personnel# - $HOME#" | awk -F' - /' '{print "/"$2}' > ~/nemo.dat
