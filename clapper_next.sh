c5ac8c85e1273b1067e668958e3ff599de4bdcd7
Theluga <96307393+Theluga@users.noreply.github.com>
2024-12-22 18:11:25 -0300

    Add files via upload

---
+++ clapper_next.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+shopt -s extglob
+
+# list of extensions for searching in current directory
+extensions_video='avi|mp4|mkv|m4v|mov|mpg|mpeg|wmv|3gp'
+extensions_audio='ogg|opus|flac|m4a|mp3|wav'
+extensions="@(${extensions_video}|${extensions_audio})"
+
+# kill other instances of clapper
+killall clapper; sleep 0.1
+
+# launch empty clapper if no argument provided
+if [ -z "$1" ]; then
+	clapper; exit
+fi
+
+# parse argument
+filename=$(realpath -- "$1")
+dirname=$(dirname "$filename")
+basename=$(basename "$filename")
+
+# list files with matching extension
+OLDIFS="$IFS"
+IFS='' list=$(ls "${dirname}"/*.${extensions} -1 2>/dev/null)
+IFS="$OLDIFS"
+
+# get position of filename in current directory
+pos=$(echo "$list" | grep -n -F -- "${basename}" | cut -d: -f1)
+
+# if the filename does not have one of the extension above, launch clapper with provided filename
+if [ -z "$pos" ]; then
+	clapper -- "${filename}"
+	exit
+fi
+
+# change positions in playlist such as the first element is the opened file
+n=$(echo "$list" | wc -l)
+echo "$list" | tail -n$(($n-$pos+1)) >  /tmp/clapper.m3u
+echo "$list" | head -n$(($pos-1))    >> /tmp/clapper.m3u
+
+# launch playlist
+IFS=$'\n'; read -d '' -r -a files < /tmp/clapper.m3u; clapper "${files[@]}"
